7e3cc2015713fe5d415b8c9976b2689881fee59b
Paul Hayes <paul.hayes@entropedia.co.uk>
2025-09-16 16:48:29 +0100

    Removed common js version of preload

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,14 +2,21 @@ import { contextBridge, ipcRenderer } from 'electron'
 
 export type IElectronAPI = {
   openOAuthUrl: (url: string) => Promise<void>
-  onGoogleAuthCallback: (callback: (code: string) => void) => void
+  exchangeOAuthCode: (code: string, clientId: string, clientSecret: string) => Promise<any>
+  getOAuthTokens: () => Promise<any>
+  clearOAuthTokens: () => Promise<boolean>
+  onOAuthCodeReceived: (callback: (code: string) => void) => void
 }
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
   openOAuthUrl: (url: string) => ipcRenderer.invoke('open-oauth-url', url),
-  onGoogleAuthCallback: (callback: (code: string) => void) => {
-    ipcRenderer.on('google-auth-callback', (_, code) => callback(code))
+  exchangeOAuthCode: (code: string, clientId: string, clientSecret: string) => 
+    ipcRenderer.invoke('exchange-oauth-code', code, clientId, clientSecret),
+  getOAuthTokens: () => ipcRenderer.invoke('get-oauth-tokens'),
+  clearOAuthTokens: () => ipcRenderer.invoke('clear-oauth-tokens'),
+  onOAuthCodeReceived: (callback: (code: string) => void) => {
+    ipcRenderer.on('oauth-code-received', (_, code) => callback(code))
   }
 } as IElectronAPI)
